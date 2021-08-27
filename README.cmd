a8dbd5a8e8b6a667660cac5a3d32e3aa0d943fc8
thaisamayra <thaisamayra@hotmail.com>
2021-08-27 09:37:05 -0300

    commit inicial

---
+++ README.cmd
@@ -0,0 +1,5 @@
+Desafio DIO recriando a página da Netflix.
+
+Código feito junto com o professor durante a aula.
+Personalizei a tela inicial com filmes e séries que falam sobre espaço, criando assim a "Netflix das Galáxias".
+Adicionei link aos botões do filme principal utilizando funções de JS.
---
+++ js/extras.js
@@ -0,0 +1,7 @@
+function botaoinfo(){
+    window.open("https://www.themoviedb.org/tv/253-star-trek?language=pt-BR");
+}
+
+function botaoassistir(){
+    window.open("https://www.youtube.com/watch?v=kJ40PDl2vAM");
+}
